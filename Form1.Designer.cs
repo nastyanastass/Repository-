12286f94086deaa36bbf332931515031781f4d09
nastyanastass <167551700+nastyanastass@users.noreply.github.com>
2024-04-19 20:47:54 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class frmMass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtn = new System.Windows.Forms.TextBox();
+            this.txtm = new System.Windows.Forms.TextBox();
+            this.txtRez = new System.Windows.Forms.TextBox();
+            this.cmdStart = new System.Windows.Forms.Button();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.cmdClear = new System.Windows.Forms.Button();
+            this.dgvMass = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMass)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Кількість рядків n=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 153);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Кількість стовпців m=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 282);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Результат:";
+            // 
+            // txtn
+            // 
+            this.txtn.Location = new System.Drawing.Point(30, 66);
+            this.txtn.Name = "txtn";
+            this.txtn.Size = new System.Drawing.Size(209, 30);
+            this.txtn.TabIndex = 3;
+            // 
+            // txtm
+            // 
+            this.txtm.Location = new System.Drawing.Point(30, 208);
+            this.txtm.Name = "txtm";
+            this.txtm.Size = new System.Drawing.Size(209, 30);
+            this.txtm.TabIndex = 4;
+            // 
+            // txtRez
+            // 
+            this.txtRez.Location = new System.Drawing.Point(30, 326);
+            this.txtRez.Multiline = true;
+            this.txtRez.Name = "txtRez";
+            this.txtRez.Size = new System.Drawing.Size(394, 277);
+            this.txtRez.TabIndex = 5;
+            // 
+            // cmdStart
+            // 
+            this.cmdStart.Location = new System.Drawing.Point(30, 639);
+            this.cmdStart.Name = "cmdStart";
+            this.cmdStart.Size = new System.Drawing.Size(363, 43);
+            this.cmdStart.TabIndex = 6;
+            this.cmdStart.Text = "Обчислити";
+            this.cmdStart.UseVisualStyleBackColor = true;
+            this.cmdStart.Click += new System.EventHandler(this.cmdStart_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(817, 639);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(371, 43);
+            this.cmdExit.TabIndex = 7;
+            this.cmdExit.Text = "Завершити роботу";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // cmdClear
+            // 
+            this.cmdClear.Location = new System.Drawing.Point(421, 639);
+            this.cmdClear.Name = "cmdClear";
+            this.cmdClear.Size = new System.Drawing.Size(371, 43);
+            this.cmdClear.TabIndex = 8;
+            this.cmdClear.Text = "Очищення полів";
+            this.cmdClear.UseVisualStyleBackColor = true;
+            this.cmdClear.Click += new System.EventHandler(this.cmdClear_Click);
+            // 
+            // dgvMass
+            // 
+            this.dgvMass.AllowUserToOrderColumns = true;
+            this.dgvMass.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMass.Location = new System.Drawing.Point(449, 27);
+            this.dgvMass.Name = "dgvMass";
+            this.dgvMass.RowHeadersWidth = 51;
+            this.dgvMass.RowTemplate.Height = 24;
+            this.dgvMass.Size = new System.Drawing.Size(725, 576);
+            this.dgvMass.TabIndex = 9;
+            // 
+            // frmMass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.dgvMass);
+            this.Controls.Add(this.cmdClear);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.cmdStart);
+            this.Controls.Add(this.txtRez);
+            this.Controls.Add(this.txtm);
+            this.Controls.Add(this.txtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmMass";
+            this.Text = "Двовимірні масиви";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMass)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtn;
+        private System.Windows.Forms.TextBox txtm;
+        private System.Windows.Forms.TextBox txtRez;
+        private System.Windows.Forms.Button cmdStart;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.Button cmdClear;
+        private System.Windows.Forms.DataGridView dgvMass;
+    }
+}
+
